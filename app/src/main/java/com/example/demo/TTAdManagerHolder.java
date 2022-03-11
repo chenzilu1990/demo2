6af7bee8fe75c2fcc901fee6cc87398e71b8425f
mway <myway1Q84@qq.com>
2022-03-11 11:36:12 +0800

    no message

--- app/src/main/java/com/example/demo/TTAdManagerHolder.java
+++ app/src/main/java/com/example/demo/TTAdManagerHolder.java
@@ -58,11 +58,11 @@ public class TTAdManagerHolder {
 
         return new TTAdConfig.Builder()
                 .appId("5279627")
-                .useTextureView(false) //使用TextureView控件播放视频,默认为SurfaceView,当有SurfaceView冲突的场景，可以使用TextureView
+                .useTextureView(true) //使用TextureView控件播放视频,默认为SurfaceView,当有SurfaceView冲突的场景，可以使用TextureView
                 .allowShowNotify(true) //是否允许sdk展示通知栏提示
                 .debug(true) //测试阶段打开，可以通过日志排查问题，上线时去除该调用
                 .directDownloadNetworkType(TTAdConstant.NETWORK_STATE_WIFI, TTAdConstant.NETWORK_STATE_3G) //允许直接下载的网络状态集合
-                .supportMultiProcess(false)//是否支持多进程
+                .supportMultiProcess(true)//是否支持多进程
                 .needClearTaskReset()
                 .build();
     }
